fe93712d9e8b75ccc934e24f2eb43ee4434c0de1
AliAlshehri033 <s3762865@student.rmit.edu.au>
2020-10-06 20:23:48 +0300

    update userProfile, userCarsReco, userRegis and add findCarPage

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
-function createNavBar() {
-    const html = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="#">Car share</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="${"/index.html"}">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="${'/register.html'}">Register</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Log In</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>`
-    const wrapper = document.querySelector('.wrapper');
-    wrapper.insertAdjacentHTML('afterbegin', html)
-    console.log('Running')
-}
-createNavBar()
+// function createNavBar() {
+//     const html = `<nav class="navbar navbar-expand-lg navbar-light bg-light">
+//             <a class="navbar-brand" href="#">Car share</a>
+//             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+//                 aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+//                 <span class="navbar-toggler-icon"></span>
+//             </button>
+//             <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+//                 <ul class="navbar-nav">
+//                     <li class="nav-item active">
+//                         <a class="nav-link" href="${"/index.html"}">Home <span class="sr-only">(current)</span></a>
+//                     </li>
+//                     <li class="nav-item">
+//                         <a class="nav-link" href="${'/UserProfile.html'}">User Profile</a>
+//                     </li>
+//                     <li class="nav-item">
+//                         <a class="nav-link" href="#">Log In</a>
+//                     </li>
+//                 </ul>
+//             </div>
+//         </nav>`
+//     const wrapper = document.querySelector('.wrapper');
+//     wrapper.insertAdjacentHTML('afterbegin', html)
+//     console.log('Running')
+// }
+// createNavBar()
 
 
 
